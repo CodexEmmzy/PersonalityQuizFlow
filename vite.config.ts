33ff388e02dfe5d3576e6f4d6d877bcad4435999
Charles Emmanuel <emmaxcharles123@gmail.com>
2025-05-10 22:35:18 +0100

    Changes to be committed: 	modified:   client/src/App.tsx 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,16 +11,16 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
